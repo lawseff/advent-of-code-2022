4c993bf9b8029edb22738271dc64d77bf08985dc
Mikhail Loseu <mikhail.loseu@gmail.com>
2023-12-03 00:00:00 +0300

    Add day 2 solution

--- src/main/kotlin/io/github/lawseff/adventofcode/challenge/parsing/BagCubeProblemSolver.kt
+++ src/main/kotlin/io/github/lawseff/adventofcode/challenge/parsing/BagCubeProblemSolver.kt
@@ -2,34 +2,40 @@ package io.github.lawseff.adventofcode.challenge.parsing
 
 import io.github.lawseff.adventofcode.Day
 import io.github.lawseff.adventofcode.PartOne
+import io.github.lawseff.adventofcode.PartTwo
 
 @Day(2)
 class BagCubeProblemSolver {
 
-    @PartOne
-    fun sumGameIdsOfPossibleGames(input: List<String>): Int {
-        val games = input.map { mapToGame(it) }
-        return games.filter { game ->
-            return@filter game.cubeSets.sumOf { it.redCount } <= 12
-                    && game.cubeSets.sumOf { it.greenCount } <= 13
-                    && game.cubeSets.sumOf { it.blueCount } <= 14
-//            val totalGreen = game.cubeSets.sumOf { it.greenCount }
-//            val totalBlue = game.cubeSets.sumOf { it.blueCount }
-        }.sumOf { game -> game.gameId }
+    companion object {
+        const val MAX_RED_COUNT = 12
+
+        const val MAX_GREEN_COUNT = 13
+
+        const val MAX_BLUE_COUNT = 14
     }
 
-    private fun mapToGame(input: String): CubeGame {
-        val withoutPrefix = input.removePrefix("Game ")
-        val gameIdAndSets = withoutPrefix.split(":")
-        val gameId = gameIdAndSets[0].toInt()
-        val sets = gameIdAndSets[1].split(";").map { set ->
-            val countByColor = set.split(",").map { it.trim() }
-            val red = countByColor.firstOrNull { it.contains("red") }?.split(" ")?.get(0)?.toInt() ?: 0
-            val green = countByColor.firstOrNull { it.contains("green") }?.split(" ")?.get(0)?.toInt() ?: 0
-            val blue = countByColor.firstOrNull { it.contains("blue") }?.split(" ")?.get(0)?.toInt() ?: 0
-            return@map CubeSet(red, green, blue)
-        }
-        return CubeGame(gameId, sets)
+    private val parser = CubeGameParser()
+
+    private val calculator = CubeGameCalculator()
+
+    @PartOne
+    fun sumGameIdsOfPossibleGames(input: List<String>): Int {
+        return input.map { parser.parse(it) }
+            .filter { isPlausibleGame(it) }
+            .sumOf { it.gameId }
+    }
+
+    @PartTwo
+    fun sumGameScores(input: List<String>): Int {
+        return input.map { parser.parse(it) }
+            .sumOf { calculator.getGameScore(it) }
+    }
+
+    private fun isPlausibleGame(game: CubeGame): Boolean {
+        return calculator.getMaxRedCount(game) <= MAX_RED_COUNT
+                && calculator.getMaxGreenCount(game) <= MAX_GREEN_COUNT
+                && calculator.getMaxBlueCount(game) <= MAX_BLUE_COUNT
     }
 
 }
--- src/main/kotlin/io/github/lawseff/adventofcode/challenge/parsing/CubeGameParser.kt
+++ src/main/kotlin/io/github/lawseff/adventofcode/challenge/parsing/CubeGameParser.kt
@@ -2,14 +2,37 @@ package io.github.lawseff.adventofcode.challenge.parsing
 
 class CubeGameParser {
 
-    // Game 6: 17 red, 2 blue, 18 green; 4 green, 10 blue, 14 red; 10 blue, 15 green, 14 red; 6 blue, 9 red; 5 blue, 7 red, 10 green
+    companion object {
+        private const val PREFIX = "Game "
+        private const val GAME_DELIMITER = ": "
+        private const val SET_DELIMITER = "; "
+        private const val CUBE_DELIMITER = ", "
+        private const val RED_CUBES = " red"
+        private const val GREEN_CUBES = " green"
+        private const val BLUE_CUBES = " blue"
+    }
 
     fun parse(input: String): CubeGame {
-        TODO()
+        val withoutPrefix = input.removePrefix(PREFIX)
+        val gameIdAndSets = withoutPrefix.split(GAME_DELIMITER)
+        val gameId = gameIdAndSets[0].toInt()
+        val sets = gameIdAndSets[1].split(SET_DELIMITER)
+            .map { parseSet(it) }
+        return CubeGame(gameId, sets)
     }
 
     private fun parseSet(input: String): CubeSet {
-        TODO()
+        val splitColors = input.split(CUBE_DELIMITER)
+        val red = parseColorCount(splitColors, RED_CUBES)
+        val green = parseColorCount(splitColors, GREEN_CUBES)
+        val blue = parseColorCount(splitColors, BLUE_CUBES)
+        return CubeSet(red, green, blue)
+    }
+
+    private fun parseColorCount(splitColors: List<String>, colorSuffix: String): Int {
+        return splitColors.firstOrNull { it.contains(colorSuffix) }
+            ?.removeSuffix(colorSuffix)
+            ?.toInt() ?: 0
     }
 
 }

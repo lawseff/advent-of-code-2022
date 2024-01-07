eeb10a5c5ead63155c4f885289c89bce19a254ed
Mikhail Loseu <mikhail.loseu@gmail.com>
2024-01-07 04:09:45 +0300

    Add day 5 part 2 brute-force solution

--- src/main/kotlin/io/github/lawseff/aoc2023/challenge/range/NestedRangeMapProblemSolver.kt
+++ src/main/kotlin/io/github/lawseff/aoc2023/challenge/range/NestedRangeMapProblemSolver.kt
@@ -3,8 +3,9 @@ package io.github.lawseff.aoc2023.challenge.range
 import io.github.lawseff.aoc2023.Day
 import io.github.lawseff.aoc2023.PartOne
 import io.github.lawseff.aoc2023.PartTwo
-import io.github.lawseff.aoc2023.challenge.range.entity.Almanac
 import io.github.lawseff.aoc2023.challenge.range.entity.AlmanacEntityMapping
+import io.github.lawseff.aoc2023.challenge.range.entity.Range
+import io.github.lawseff.aoc2023.challenge.range.entity.SourceDestinationRange
 
 @Day(5)
 class NestedRangeMapProblemSolver {
@@ -14,22 +15,32 @@ class NestedRangeMapProblemSolver {
     @PartOne
     fun findMinLocationIdFromSeedIds(input: List<String>): Long {
         val almanac = parser.parseAlmanac(input)
-        val locationsForSeeds = almanac.initialSeeds.map { findLocationId(it, almanac) }
-        return locationsForSeeds.min()
+        val locations = almanac.initialSeeds.map { findLastCorrespondingId(it, almanac.entityMappings) }
+        return locations.min()
     }
 
+    // This is a brute-force solution
     @PartTwo
     fun findMinLocationIdFromSeedIdRanges(input: List<String>): Long {
-        TODO()
+        val almanac = parser.parseAlmanac(input)
+        val entityMappings = invert(almanac.entityMappings)
+        val seedRanges = almanac.initialSeeds.chunked(2)
+            .map { Range(it[0], it[1]) }
+
+        var locationId = 1L
+        while (!contains(seedRanges, findLastCorrespondingId(locationId, entityMappings))) {
+            locationId++
+        }
+        return locationId
     }
 
-    private fun findLocationId(seedId: Long, almanac: Almanac): Long {
-        return almanac.entityMappings.fold(seedId) { currentId, mapping ->
-            findCorrespondingId(currentId, mapping)
+    private fun findLastCorrespondingId(startId: Long, entityMappings: List<AlmanacEntityMapping>): Long {
+        return entityMappings.fold(startId) { currentId, mapping ->
+            findNextCorrespondingId(currentId, mapping)
         }
     }
 
-    private fun findCorrespondingId(sourceId: Long, mapping: AlmanacEntityMapping): Long {
+    private fun findNextCorrespondingId(sourceId: Long, mapping: AlmanacEntityMapping): Long {
         var correspondingId = sourceId
         for (range in mapping.ranges) {
             val sourceStartId = range.sourceStartId
@@ -41,6 +52,29 @@ class NestedRangeMapProblemSolver {
             }
         }
         return correspondingId
+    }
+
+    private fun invert(mappings: List<AlmanacEntityMapping>): List<AlmanacEntityMapping> {
+        return mappings.reversed().map { invert(it) }
+    }
+
+    private fun invert(mapping: AlmanacEntityMapping): AlmanacEntityMapping {
+        val sourceType = mapping.destinationType
+        val destinationType = mapping.sourceType
+        val ranges = mapping.ranges.map {
+            SourceDestinationRange(it.destinationStartId, it.sourceStartId, it.rangeLength)
+        }
+        return AlmanacEntityMapping(sourceType, destinationType, ranges)
+    }
+
+    private fun contains(ranges: List<Range>, id: Long): Boolean {
+        return ranges.any { contains(it, id) }
+    }
+
+    private fun contains(range: Range, id: Long): Boolean {
+        val startId = range.startId
+        val endId = startId + range.rangeLength - 1
+        return id in startId..endId
     }
 
 }
---
+++ src/main/kotlin/io/github/lawseff/aoc2023/challenge/range/entity/Range.kt
@@ -0,0 +1,3 @@
+package io.github.lawseff.aoc2023.challenge.range.entity
+
+data class Range(val startId: Long, val rangeLength: Long)
